5e2305128e2bd40054473b12f5ffd7e2dcdd68c5
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2024-07-17 17:51:42 +0530

    changed products fetching endpoint

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const productModel = require('../models/product-model');
 
-router.get('/', async (req, res, next) => {
+router.get('/', (req, res, next) => {
+    res.send('products');
+})
+
+router.get('/all', async (req, res, next) => {
     const products = await productModel.find();
     if(products) {
         res.json({message: "sucessfully got products", products: products});
